e2b8ad7943fd6614034a0ffae2f7790122f66330
r0b0t1ka <flyingeagle413@gmail.com>
2013-08-04 01:04:41 -0400

    Hours of extensive permission fixing to ensure unauthorized players cannot use command shortcuts. Bugfixing also done. (V1.5)

--- src/r0b0t1ka/CommandShortcuts/CommandShortcuts.java
+++ src/r0b0t1ka/CommandShortcuts/CommandShortcuts.java
@@ -12,6 +12,9 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.*;
 import org.bukkit.Bukkit;
+import org.bukkit.permissions.Permission;
+import org.bukkit.plugin.*;
+import org.bukkit.command.*;
 /**
  *
  * @author r0b0t1ka
@@ -101,8 +104,13 @@ public class CommandShortcuts extends JavaPlugin
                 writer.println("   " + cmdName[counter] + ":");
                 writer.println("      description: " + cmdDesc[counter]);
                 writer.println("      usage: " + cmdUsag[counter]);
-                writer.println("      permission: " + cmdPerm[counter]);
-                writer.println("      permission-message: " + cmdPerM[counter]);
+                writer.println("      permission-message: You don't have <permission>!");
+            }
+            writer.println("permissions:");
+            for (int counter = 0; counter < cmdName.length; counter++)
+            {
+                writer.println("  CommandShortcuts." + cmdPerm[counter] + ":");
+                writer.println("    description: " + cmdDesc[counter]);
             }
             getLogger().info("CommandShortcuts has successfully written the plugin.yml file!");
             getLogger().info("Place plugin.yml in jar file if it has been newly generated.");
@@ -135,6 +143,7 @@ public class CommandShortcuts extends JavaPlugin
         ArrayList<String> commandU = new ArrayList<>();//Command usage
         ArrayList<String> commandP = new ArrayList<>();//Command parameters contains values if paramaters specified in usage
         ArrayList<Boolean> commandPP = new ArrayList<>();//Command parameters present truth
+        ArrayList<String> permA = new ArrayList<>(); // Permission for actual command
         while(in.hasNextLine())
         {
             String cmds = in.nextLine();
@@ -144,6 +153,7 @@ public class CommandShortcuts extends JavaPlugin
             cmds = cmds.substring(index + 1);
             index = cmds.indexOf("|");
             String cmdActual = cmds.substring(0, index);
+            permA.add(permissionAnalyzer(cmdActual));
             commandA.add(cmdActual);
             cmds = cmds.substring(index + 1);
             perm.add("CommandShortcuts." + cmdN);
@@ -167,10 +177,11 @@ public class CommandShortcuts extends JavaPlugin
         consoleT.trimToSize();
         playerT.trimToSize();
         commandU.trimToSize();
+        permA.trimToSize();
         for (int counter = 0; counter < commandU.size(); counter++)
         {
             int index = commandU.get(counter).indexOf("<");
-            if (!(index == -1) && commandU.get(counter).substring(0, index).equals(commandS.get(counter)))
+            if (!(index == -1))
             {
                 String cmditself = commandU.get(counter);
                 String param = commandU.get(counter);
@@ -209,24 +220,34 @@ public class CommandShortcuts extends JavaPlugin
                 else
                 {
                     Player player = (Player) sender;
-                    if (player.hasPermission(perm.get(counter)) && playerT.get(counter))
+                    if (player.isOp() && playerT.get(counter))
                     {
-                            player.performCommand(commandA.get(counter));
-                            command = true;
+                        player.performCommand(commandA.get(counter));
+                        command = true;
                     }
-                    else if (player.hasPermission(perm.get(counter)) && !(playerT.get(counter)))
+                    else if (player.hasPermission(perm.get(counter)) && playerT.get(counter) && player.hasPermission(permA.get(counter)))
+                    {
+                        player.performCommand(commandA.get(counter));
+                        command = true;
+                    }
+                    else if (!(playerT.get(counter)))
                     {
                         sender.sendMessage("This command is for the console only");
                         command = false;
                     }
-                    else if (!player.hasPermission(perm.get(counter)) && !(playerT.get(counter)))
+                    else if (player.hasPermission(perm.get(counter)) && !player.hasPermission(permA.get(counter)))
                     {
-                        sender.sendMessage("You don't have permission to use this and it's for the console only.");
+                        sender.sendMessage("You don't have permission to use the original command.");
                         command = false;
                     }
-                    else if (!player.hasPermission(perm.get(counter)) && !(playerT.get(counter)))
+                    else if (!player.hasPermission(perm.get(counter)) && !player.hasPermission(permA.get(counter)))
                     {
-                        sender.sendMessage("You don't have permission to use this command.");
+                        sender.sendMessage("You don't have permission to use the original command and/or this command shortcut:");
+                        command = false;
+                    }
+                    else if (!player.hasPermission(perm.get(counter)) && player.hasPermission(permA.get(counter)))
+                    {
+                        sender.sendMessage("You don't have permission to use this command shortcut.");
                         command = false;
                     }
                 }
@@ -234,5 +255,185 @@ public class CommandShortcuts extends JavaPlugin
         }
         in.close();
         return command;
-    }    
+    }
+    private String permissionAnalyzer(String cmdActual)
+    {
+        String permissionz = cmdActual;
+        String permissionString = "";
+        int index = permissionz.indexOf("<");
+        Plugin[] pl = Bukkit.getServer().getPluginManager().getPlugins();
+        ArrayList<String> permissionOverLayList = new ArrayList<>(pl.length);
+        ArrayList<String> commandOverLayList = new ArrayList<>(pl.length);
+        SimpleCommandMap commandMap = new SimpleCommandMap(getServer());
+        Collection<Command> cmdCollection = commandMap.getCommands();
+        Object[] commandArray = cmdCollection.toArray();
+        for (int counter = 0; counter < pl.length; counter++)
+        {
+            List permissionUnderLayList = pl[counter].getDescription().getPermissions();
+            if (!permissionUnderLayList.isEmpty())
+            {
+                String[] permissionBeta = new String[permissionUnderLayList.size()];
+                for (int bounter = 0; bounter < permissionBeta.length; bounter++)
+                {
+                    Permission permeator = (Permission)permissionUnderLayList.get(bounter);
+                    String permissionAlpha = permeator.getName();
+                    permissionOverLayList.add(permissionAlpha);
+                }
+            }
+        }
+        permissionOverLayList.trimToSize();
+        if (index == -1)
+        {
+            if (permissionz.equals("version") || permissionz.equals("plugins") || permissionz.equals("reload") || permissionz.equals("timings") || permissionz.equals("kill") || permissionz.equals("me") || permissionz.equals("banlist") || permissionz.equals("help") || permissionz.equals("?") || permissionz.equals("banlist") || permissionz.equals("stop") || permissionz.equals("save-all") || permissionz.equals("save-off") || permissionz.equals("save-on") || permissionz.equals("list") || permissionz.equals("whitelist list") || permissionz.equals("whitelist reload") || permissionz.equals("toggledownfall") || permissionz.equals("seed") || permissionz.equals("gamerule"))
+            {
+                permissionString = "bukkit.command.";
+                switch (permissionz) {
+                    case "version":
+                        permissionString += "version";
+                        break;
+                    case "plugins":
+                        permissionString += "plugins";
+                        break;
+                    case "reload":
+                        permissionString += "reload";
+                        break;
+                    case "timings":
+                        permissionString += "timings";
+                        break;
+                    case "kill":
+                        permissionString += "kill";
+                        break;
+                    case "me":
+                        permissionString += "me";
+                        break;
+                    case "banlist":
+                        permissionString += "ban.list";
+                        break;
+                    case "stop":
+                        permissionString += "stop";
+                        break;
+                    case "save-all":
+                        permissionString += "save.perform";
+                        break;
+                    case "save-off":
+                        permissionString += "save.disable";
+                        break;
+                    case "save-on":
+                        permissionString += "save.enable";
+                        break;
+                    case "list":
+                        permissionString += "list";
+                        break;
+                    case "whitelist list":
+                        permissionString += "whitelist.list";
+                        break;
+                    case "whitelist reload":
+                        permissionString += "whitelist.reload";
+                        break;
+                    case "toggledownfall":
+                        permissionString += "toggledownfall";
+                        break;
+                    case "seed":
+                        permissionString += "seed";
+                        break;
+                    case "gamerule":
+                        permissionString += "gamerule";
+                        break;
+                }
+            }
+            else
+            {
+                for(int counter = 0; counter < commandArray.length; counter++)
+                {
+                    if(permissionz.equals(commandArray[counter]))
+                        permissionString = permissionOverLayList.get(counter);
+                }
+            }
+        }
+        else 
+        {
+            permissionz = permissionz.substring(0, index);
+            if (permissionz.equals("tell") || permissionz.equals("kick") || permissionz.equals("ban") || permissionz.equals("pardon") || permissionz.equals("ban-ip") || permissionz.equals("pardon-ip") || permissionz.equals("op") || permissionz.equals("deop") || permissionz.equals("tp") || permissionz.equals("give") || permissionz.equals("say") || permissionz.equals("whitelist") || permissionz.equals("time") || permissionz.equals("gamemode") || permissionz.equals("defaultgamemode") || permissionz.equals("enchant") || permissionz.equals("weather") || permissionz.equals("clear") || permissionz.equals("difficulty") || permissionz.equals("spawnpoint") || permissionz.equals("effect"))
+            {
+                permissionString = "bukkit.command.";
+                switch (permissionz) {
+                    case "tell":
+                        permissionString += "tell";
+                        break;
+                    case "kick":
+                        permissionString += "kick";
+                        break;
+                    case "ban":
+                        permissionString += "ban.player";
+                        break;
+                    case "pardon":
+                        permissionString += "unban.player";
+                        break;
+                    case "ban-ip":
+                        permissionString += "ban.ip";
+                        break;
+                    case "pardon-ip":
+                        permissionString += "unban.ip";
+                        break;
+                    case "op":
+                        permissionString += "op.give";
+                        break;
+                    case "deop":
+                        permissionString += "op.take";
+                        break;
+                    case "tp":
+                        permissionString += ".teleport";
+                        break;
+                    case "give":
+                        permissionString += ".give";
+                        break;
+                    case "say":
+                        permissionString += ".say";
+                        break;
+                    case "whitelist":
+                        permissionString += ".whitelist";
+                        break;
+                    case "time":
+                        permissionString += ".time";
+                        break;
+                    case "gamemode":
+                        permissionString += ".gamemode";
+                        break;
+                    case "xp":
+                        permissionString += ".xp";
+                        break;
+                    case "defaultgamemode":
+                        permissionString += ".defaultgamemode";
+                        break;
+                    case "enchant":
+                        permissionString += ".enchant";
+                        break;
+                    case "weather":
+                        permissionString += ".weather";
+                        break;
+                    case "clear":
+                        permissionString += ".clear";
+                        break;
+                    case "difficulty":
+                        permissionString += ".difficulty";
+                        break;
+                    case "spawnpoint":
+                        permissionString += ".spawnpoint";
+                        break;
+                    case "effect":
+                        permissionString += ".effect";
+                        break;
+                }
+            }
+            else
+            {
+                for (int counter = 0; counter < commandArray.length; counter++)
+                {
+                    if(permissionz.equals(commandArray[counter]))
+                        permissionString = permissionOverLayList.get(counter);
+                }
+            }
+        }
+        return permissionString;
+    }
 }
